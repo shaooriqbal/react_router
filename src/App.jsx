e3fdafe5851b18e7184f0743857d8dc6bf2751af
shaoor <shaooriqbal786@gmail.com>
2023-08-14 13:02:01 +0500

    nested routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { createBrowserRouter, Routes, Route, Link, NavLink, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import RootLayout from './layout/RootLayout';
+import HelpLayout from './layout/HelpLayout';
+import Faq from './pages/Help/faq';
+import Contact from './pages/Help/contact';
+import NotFound from './pages/NotFound';
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={ <RootLayout/>}>
+          <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path ="help" element={<HelpLayout />}>
+        <Route path="faq"  element ={ <Faq/>}/>
+        <Route path="contact" element={ <Contact/>} />
+      </Route >
+      <Route path="*" element={ <NotFound/>} />
+          </Route>
+  )
+);
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+ return (
+    <RouterProvider router={router}/>
   )
 }
 
---
+++ src/layout/HelpLayout.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { NavLink, Outlet } from 'react-router-dom'
+
+export default function HelpLayout() {
+  return (
+      <div className='help'>
+           <h2>Help Center</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum dolor assumenda, itaque nesciunt alias voluptatum nobis blanditiis eos officiis aperiam earum eum vel quas odio optio, distinctio ab sunt unde incidunt ipsum omnis amet magnam accusantium aut! Excepturi, cupiditate iusto!</p>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus veniam ducimus eligendi nihil, cumque ab eveniet modi architecto quidem, non odit saepe facere voluptas esse mollitia illo fuga exercitationem id dicta iusto eaque numquam quaerat ad! Fugit velit beatae laudantium.</p>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus veniam ducimus eligendi nihil, cumque ab eveniet modi architecto quidem, non odit saepe facere voluptas esse mollitia illo fuga exercitationem id dicta iusto eaque numquam quaerat ad! Fugit velit beatae laudantium.</p>
+          <nav>
+              <NavLink to="faq"> 
+                  View the FAQ
+              </NavLink>
+              <NavLink to="contact">
+                  Contact Us
+              </NavLink>
+          </nav>
+          <Outlet />
+      </div>
+  )
+}
